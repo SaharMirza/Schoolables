0604242465eb925652de1717dcba661791758042
abc <khaulah.ahmed@gmail.com>
2022-11-16 13:03:46 +0500

    Child/Children Profile Screens

---
+++ lib/views/Main Screen Pages/Profile Pages/ChildProfile.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:flutterdemo/constants/colors.dart';
+import 'package:flutterdemo/constants/fonts.dart';
+import 'package:flutterdemo/views/Main%20Screen%20Pages/Widgets/my_profile.dart';
+import 'package:flutterdemo/views/Widgets/textfield.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+class ChildProfileScreen extends StatefulWidget {
+  const ChildProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChildProfileScreen> createState() => _ChildProfileScreenState();
+}
+
+class _ChildProfileScreenState extends State<ChildProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenWidth = MediaQuery.of(context).size.width;
+
+    final TextEditingController _namecontroller = TextEditingController();
+
+    return Scaffold(
+      body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height*0.03,
+                ),
+                NavigateBackWidget(
+                  screenWidth: screenWidth,
+                  screenHeight: screenHeight,
+                ),
+                SizedBox(
+                  height: screenHeight*0.03,
+                ),
+                Padding(
+                  padding:  EdgeInsets.symmetric(horizontal: 26),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            "Child Profile",
+                            textAlign: TextAlign.left,
+                            style: GoogleFonts.poppins(
+                                color:Colors.black,
+                                fontSize: screenHeight * 0.030,
+                                fontWeight: FontWeight.w600),
+                          ),  Spacer(),
+                          CircleAvatar(
+                            radius: 34,
+                            backgroundImage: AssetImage('assets/images/profile.png'),
+                          ),
+                        ],
+                      ),
+
+                      SizedBox(height: screenHeight*0.03,),
+                         _reuseableColumn('Name'),
+                       SizedBox(height: screenHeight*0.03),
+                       _reuseableColumn('School'),
+                      SizedBox(height: screenHeight*0.03),
+                      _reuseableColumn('Grade'),
+                      SizedBox(height: screenHeight*0.03),
+                         Row(
+                         children: [
+                           Text(
+                           "Add Child",
+                           textAlign: TextAlign.left,
+                           style: GoogleFonts.poppins(
+                               color: const Color.fromARGB(255, 59, 59, 61),
+                               fontSize: screenHeight * 0.025,
+                               fontWeight: FontWeight.w600),
+                         ),
+                          Spacer(),
+                          Icon(Icons.add_circle,
+                          size: 50,
+                            color: MyColors.buttonColor,
+                          ),
+                        ],
+                      ),
+                     ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+    );
+  }
+
+  Widget _reuseableColumn(String title){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: GoogleFonts.poppins(
+              fontWeight: FontWeight.w500,
+              fontSize: 12
+          ),
+          textAlign: TextAlign.left,
+        ),
+        TextField(
+          decoration: InputDecoration(contentPadding: EdgeInsets.only(top: 10)),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/views/Main Screen Pages/Profile Pages/ChildrenProfileScreen.dart
@@ -0,0 +1,223 @@
+import 'package:flutter/material.dart';
+import 'package:flutterdemo/constants/colors.dart';
+import 'package:flutterdemo/views/Main%20Screen%20Pages/Profile%20Pages/CreateChildrenProfile.dart';
+import 'package:flutterdemo/views/Main%20Screen%20Pages/Widgets/my_profile.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../../Classes/Children.dart';
+
+class ChildernProfileScreen extends StatefulWidget {
+  const ChildernProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChildernProfileScreen> createState() => _ChildernProfileScreenState();
+}
+
+class _ChildernProfileScreenState extends State<ChildernProfileScreen> {
+  @override
+  List<Children> children = [
+    Children(name: "Samra Noman", school: "Beaconhouse", grade: "4th Grade"),
+    Children(name: "Omar Noman", school: "Beaconhouse", grade: "1st Grade")
+  ];
+
+  Widget build(BuildContext context) {
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenWidth = MediaQuery.of(context).size.width;
+    // TODO: implement build
+    return Scaffold(
+      body: Container(
+        height: screenHeight,
+        width: screenWidth,
+        child: Column(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                NavigateBackWidget(
+                  screenWidth: screenWidth,
+                  screenHeight: screenHeight,
+                ),
+              ],
+            ),
+            Container3(),
+            SizedBox(
+              height: screenHeight * 0.05,
+            ),
+            MyProfileTextWidget(
+                text: "Children Profiles", screenWidth: screenWidth),
+            SizedBox(
+              height: screenHeight * 0.04,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ListView.separated(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                itemCount: children.length,
+                separatorBuilder: (context, int index) =>
+                    SizedBox(height: screenHeight * 0.01),
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        ListCard(
+                          children: children[index],
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(
+              height: screenHeight * 0.05,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.add_circle,
+                  size: 20,
+                  color: MyColors.buttonColor,
+                ),
+                Text(
+                  'Add More',
+                  style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w500,
+                      ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ListCard extends StatelessWidget {
+  final Children children;
+
+  const ListCard({super.key, required this.children});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenWidth = MediaQuery.of(context).size.width;
+
+    //int size = coursebooks.length;
+    return Container(
+      height: screenHeight * 0.15,
+      width: screenWidth * 0.8,
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(242, 232, 228, 100),
+        border: Border.all(color: Colors.white),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Column(
+                  children: [
+                    CircleAvatar(
+                      radius: 28,
+                      backgroundImage: AssetImage('assets/images/profile.png'),
+                    ),
+                  ],
+                ),
+                SizedBox(width: screenWidth * 0.02),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      children.name,
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.poppins(
+                          color: const Color.fromARGB(255, 59, 59, 61),
+                          fontSize: screenHeight * 0.025,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      children.school,
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.poppins(
+                        color: const Color.fromARGB(255, 59, 59, 61),
+                        fontSize: screenHeight * 0.02,
+                      ),
+                    ),
+                    Text(
+                      children.grade,
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.poppins(
+                        color: const Color.fromARGB(255, 59, 59, 61),
+                        fontSize: screenHeight * 0.02,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 1.0, right: 9.0),
+                child: InkWell(
+                  child: Icon(
+                    Icons.arrow_forward_ios,
+                    size: 18,
+                    color: Colors.blueGrey,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Container3 extends StatelessWidget {
+  const Container3({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenWidth = MediaQuery.of(context).size.width;
+
+    return SizedBox(
+      height: 120,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //logo
+          Image.asset(
+            'images/logo.png',
+            height: 100,
+            width: 100,
+          ),
+          const Text("Schoolables",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 15,
+                color: Colors.black,
+              )),
+        ],
+      ),
+    );
+  }
+}
