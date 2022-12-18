a7ade1e32cac108e257367e9f175cc5b6d653ae9
Khaulah Ahmed <khaulah.ahmed@gmail.com>
2022-12-18 15:29:40 +0500

    clean code

--- lib/views/Main Screen Pages/Search.dart
+++ lib/views/Main Screen Pages/Search.dart
@@ -20,62 +20,59 @@ class _SearchPageState extends State<SearchPage> {
     // TODO: implement initState
     super.initState();
     //newList = widget.searchList;
-    newList=[];
+    newList = [];
   }
 
   @override
   Widget build(BuildContext context) {
     final double screenHeight = MediaQuery.of(context).size.height;
     final double screenWidth = MediaQuery.of(context).size.width;
-    String val='';
+    String val = '';
     final textController = TextEditingController();
 
     return Scaffold(
-      body: (
-          Column(
-            children: [
-              const HeaderBar(title: "Search"),
-              SizedBox(height: screenHeight*0.03,),
-              SizedBox(
-                width: screenWidth*0.9,
-                child: TextField(
-                  autofocus: true,
-                  onChanged: (value) {
-                    val = value;
-                    if(val!='' ) {
-                      newList = widget.searchList
-                          .where(((products) =>
-                      (products.title
+      body: (Column(
+        children: [
+          const HeaderBar(title: "Search"),
+          SizedBox(
+            height: screenHeight * 0.03,
+          ),
+          SizedBox(
+            width: screenWidth * 0.9,
+            child: TextField(
+              autofocus: true,
+              onChanged: (value) {
+                val = value;
+                if (val != '') {
+                  newList = widget.searchList
+                      .where(((products) => (products.title
                           .toLowerCase()
                           .contains(value.toLowerCase()))))
-                          .toList();
-                    }
-                    else{
-                      newList=[];
-                    }
-                    setState(() {});
-                  },
-                  decoration: InputDecoration(
-                    prefixIcon: const Icon(Icons.search_outlined),
-                    filled: true,
-                    fillColor: const Color.fromRGBO(242, 233, 228, 1.0),
-                    labelText: "What are you looking for?",
-                    enabledBorder: OutlineInputBorder(
-                      borderSide:
-                      const BorderSide(color: Color.fromRGBO(242, 233, 228, 1.0)),
-                      borderRadius: BorderRadius.circular(50.0),
-                    ),
-                  ),
+                      .toList();
+                } else {
+                  newList = [];
+                }
+                setState(() {});
+              },
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.search_outlined),
+                filled: true,
+                fillColor: const Color.fromRGBO(242, 233, 228, 1.0),
+                labelText: "What are you looking for?",
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(
+                      color: Color.fromRGBO(242, 233, 228, 1.0)),
+                  borderRadius: BorderRadius.circular(50.0),
                 ),
               ),
-              SizedBox(height:screenHeight*0.01 ,),
-              Expanded(
-                child: SearchListView(newList:newList)
-              ),
-            ],
-          )),
+            ),
+          ),
+          SizedBox(
+            height: screenHeight * 0.01,
+          ),
+          Expanded(child: SearchListView(newList: newList)),
+        ],
+      )),
     );
   }
 }
-
-
